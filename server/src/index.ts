9c64959c9b3e8fc3fce07fcb5a12fca110bf77f5
MeatBxll <anthonymancini27@gmail.com>
2025-06-04 19:01:31 -0400

    messing around with prisma a little

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,13 @@ async function main() {
   console.log(users);
 }
 
+async function findUser(user: any) {
+  const users = await prisma.user.findMany();
+  const me = users.map((e: any) => {
+    user === e ? e : null;
+  });
+}
+
 main()
   .catch(console.error)
   .finally(() => prisma.$disconnect());
