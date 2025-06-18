e1aa9ec8873d9472346542bdb0c607fec46f5899
MeatBxll <anthonymancini27@gmail.com>
2025-06-18 18:04:03 -0400

    studying but not much time to make progress today

--- server/src/routes/messageRoutes.ts
+++ server/src/routes/messageRoutes.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post("/", authenticateToken, createMessage);
 router.get("/", authenticateToken, getMessages);
+router.get("/", authenticateToken);
 
 export default router;
